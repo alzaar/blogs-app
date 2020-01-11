01dee04af0009f3af212dfdf759c2a9f05f5d01b
alizahir@live.com <alizahir@live.com>
2020-01-11 16:08:01 +0500

    FC

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  window.fetch('blogs/')
+  .then(function(response) {
+    // The response is a Response instance.
+    // You parse the data into a useable format using `.json()`
+    return response.json();
+  }).then(function(data) {
+    // `data` is the parsed version of the JSON returned from the above endpoint.
+    console.log(data);  // { "userId": 1, "id": 1, "title": "...", "body": "..." }
+  });
   return (
     <div className="App">
       <header className="App-header">
