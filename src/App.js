f6b2f3df415e84df9e62431111a7f3b364693f7d
alizahir@live.com <alizahir@live.com>
2020-01-12 09:26:24 +0500

    adds http calling agent and tests agent

--- src/App.js
+++ src/App.js
@@ -1,35 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import './App.css'
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
 
-function App() {
-  window.fetch('blogs/')
-  .then(function(response) {
-    // The response is a Response instance.
-    // You parse the data into a useable format using `.json()`
-    return response.json();
-  }).then(function(data) {
-    // `data` is the parsed version of the JSON returned from the above endpoint.
-    console.log(data);  // { "userId": 1, "id": 1, "title": "...", "body": "..." }
-  });
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+  onSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    let params = {
+      params: {
+        username: 'ali',
+        password: 'ali'
+      }
+    }
+    axios.post('login/', {
+      "username":"",
+      "password":""
+      }).then(res => console.log(res))
+
+      // axios({
+      //   method: 'post',
+      //   url: 'login/',
+      //   data: {
+      //     email : "test@gmail.com",
+      //     username : 'test'
+      //   }
+      // })
+      // .then(response => (console.log(response)))
+  }
+  // window.fetch('blogs/')
+  // .then(function(response) {
+  //   // The response is a Response instance.
+  //   // You parse the data into a useable format using `.json()`
+  //   return response.json();
+  // }).then(function(data) {
+  //   // `data` is the parsed version of the JSON returned from the above endpoint.
+  //   console.log(data);  // { "userId": 1, "id": 1, "title": "...", "body": "..." }
+  // });
+  render () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form  onSubmit={this.onSubmit}>
+        <label>Name</label>
+        <input type='text' name='username' onChange={this.onChange} />
+        <label>Password</label>
+        <input type='password' name='password' onChange={this.onChange} />
+        <input type='submit' />
+      </form>
     </div>
   );
+  }
 }
 
 export default App;
