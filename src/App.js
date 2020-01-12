232f290a2c3d0ad126a6249a309ab462499aafa5
alizahir@live.com <alizahir@live.com>
2020-01-12 10:18:57 +0500

    Adds login form component

--- src/App.js
+++ src/App.js
@@ -1,69 +1,17 @@
 import React from 'react'
 import axios from 'axios'
 import './App.css'
+import LoginForm from './components/LoginForm'
+
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      username: '',
-      password: ''
-    }
-  }
-  onSubmit = (e) => {
-    e.preventDefault()
-    console.log(this.state)
-  }
-
-  onChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-    let params = {
-      params: {
-        username: 'ali',
-        password: 'ali'
-      }
-    }
-    axios.post('login/', {
-      "username":"",
-      "password":""
-      }).then(res => console.log(res))
-
-      // axios({
-      //   method: 'post',
-      //   url: 'login/',
-      //   data: {
-      //     email : "test@gmail.com",
-      //     username : 'test'
-      //   }
-      // })
-      // .then(response => (console.log(response)))
-  }
-  // window.fetch('blogs/')
-  // .then(function(response) {
-  //   // The response is a Response instance.
-  //   // You parse the data into a useable format using `.json()`
-  //   return response.json();
-  // }).then(function(data) {
-  //   // `data` is the parsed version of the JSON returned from the above endpoint.
-  //   console.log(data);  // { "userId": 1, "id": 1, "title": "...", "body": "..." }
-  // });
-  render () {
+function App(){
   return (
     <div>
-      <form  onSubmit={this.onSubmit}>
-        <label>Name</label>
-        <input type='text' name='username' onChange={this.onChange} />
-        <label>Password</label>
-        <input type='password' name='password' onChange={this.onChange} />
-        <input type='submit' />
-      </form>
+      <LoginForm />
     </div>
   );
-  }
 }
 
 export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+  onSubmit = (e) => {
+    e.preventDefault()
+    let params = {
+      username: this.state.username,
+      password: this.state.password
+    }
+    axios.post('login/', params).then(res => console.log(res))
+  }
+
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  render() {
+    return (
+    <div>
+      <form  onSubmit={this.onSubmit}>
+        <label>Name</label>
+        <input type='text' name='username' onChange={this.onChange} />
+        <label>Password</label>
+        <input type='password' name='password' onChange={this.onChange} />
+        <input type='submit' />
+      </form>
+    </div>
+    )
+  }
+}
+
+export default LoginForm
