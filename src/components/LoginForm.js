2b11aed14a8aa265990900778807df1007cd00ea
alizahir@live.com <alizahir@live.com>
2020-01-14 13:01:46 +0500

    Removes commented code

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -25,19 +25,7 @@ class LoginForm extends Component {
     })
   }
   render() {
-  //   let params = {
-  //     username: 'ali',
-  //     password: 'ali'
-  //   })
-  // }
-    let headers = {
-      'Authroization': 'Token 84cd72f2675b5991441a24e0808bdf37dba9b1b763521044ec32f00524ef53e6'
-    }
-    console.log(getDecryptData()[0].token)
-    //  { headers: headers }
-    // axiosInstance.post('login/', params).then(res => encryptData(res.data.token))
-    axiosInstance.get('blogs/').then(res => console.log(res))
-    
+    // axiosInstance.get('blogs/').then(res => console.log(res))
     return (
     <div>
       <form  onSubmit={this.onSubmit}>
